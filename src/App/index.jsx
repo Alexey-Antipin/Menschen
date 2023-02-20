3478ceefcdee0c55fe13120e21d56ae82bd49aa1
Alexey <stierlitzotto21@gmail.com>
2023-02-20 22:25:07 +0300

    редактирование

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,25 +1,25 @@
 import "./index.scss";
-import webovio from "../image/slider/webovio.png";
-import webovioPoint from "../image/slider/webovioPoint.png";
-import triangleLeft from "../image/slider/triangleLeft.png";
-import rectangleCenter from "../image/slider/rectangle.png";
-import triangleRight from "../image/slider/triangleRight.png";
-import getInTouch from "../image/slider/getInTouch.png";
-import menu from "../image/slider/menu.png";
-
-import wal from "../image/clientBlock/wal.png";
-import ideal from "../image/clientBlock/ideal.png";
-import leadershipAcademy from "../image/clientBlock/leadershipAcademy.png";
-import andrewPorter from "../image/clientBlock/andrewPorter.png";
-import phalenLeadership from "../image/clientBlock/phalenLeadership.png";
-
-import photoPeople from "../image/clientService/oval.png";
-import bitmap from "../image/project/bitmap_1.png";
-import bitmap_2 from "../image/project/bitmap_2.png";
-import bitmap_3 from "../image/project/bitmap_3.png";
-import peopleBanner from "../image/project/people.png";
-import peopleBanner_2 from "../image/project/people_2.png";
-import peopleBanner_3 from "../image/project/people_3.png";
+import {
+  triangleRight,
+  webovioPoint,
+  triangleLeft,
+  rectangleCenter,
+  getInTouch,
+  webovio,
+  menu,
+  leadershipAcademy,
+  phalenLeadership,
+  andrewPorter,
+  ideal,
+  wal,
+  peopleBanner_2,
+  peopleBanner_3,
+  photoPeople,
+  peopleBanner,
+  bitmap_2,
+  bitmap_3,
+  bitmap,
+} from "../image";
 
 export const App = () => {
   return (
@@ -48,18 +48,10 @@ export const App = () => {
                     />
                   </div>
                   <img className="logo__text" src={webovio} alt="text" />
-                  <img
-                    className="logo__point"
-                    src={webovioPoint}
-                    alt="point"
-                  />
+                  <img className="logo__point" src={webovioPoint} alt="point" />
                 </div>
               </div>
-              <img
-                className="navigation-bar__menu"
-                src={menu}
-                alt="menu"
-              />
+              <img className="navigation-bar__menu" src={menu} alt="menu" />
             </ul>
           </nav>
 
@@ -70,8 +62,8 @@ export const App = () => {
                 A creative agency for redemptive brands
               </div>
               <div className="box__text">
-                Anteelo is a leading strategic design firm that builds
-                powerful digital solutions for startups and enterprises.
+                Anteelo is a leading strategic design firm that builds powerful
+                digital solutions for startups and enterprises.
               </div>
               <img className="box__link" src={getInTouch} alt="link"></img>
               <div className="box__string"></div>
@@ -81,11 +73,7 @@ export const App = () => {
           <div className="client">
             <div className="client__inner">
               <div className="client__box">
-                <img
-                  className="client__item"
-                  src={andrewPorter}
-                  alt="brends"
-                />
+                <img className="client__item" src={andrewPorter} alt="brends" />
                 <img
                   className="client__item"
                   src={leadershipAcademy}
@@ -112,9 +100,9 @@ export const App = () => {
                   Strategy. Design Content. Technology Development
                 </div>
                 <div className="services__text">
-                  There’s no secret sauce, no wizard behind the curtain.
-                  What makes Aerolab tick is an interdisciplinary team with
-                  a framework that fosters candid collaboration.
+                  There’s no secret sauce, no wizard behind the curtain. What
+                  makes Aerolab tick is an interdisciplinary team with a
+                  framework that fosters candid collaboration.
                 </div>
                 <div className="services__text">▶ More know About us</div>
               </div>
@@ -123,9 +111,9 @@ export const App = () => {
             <div className="services__item">
               <div className="services__inner-block">
                 <div className="services__text">
-                  With More than 10 Years of Knowledge and Expertise we
-                  Design and Code Websites and Apps, We Build Brands and
-                  Help Them Succeed
+                  With More than 10 Years of Knowledge and Expertise we Design
+                  and Code Websites and Apps, We Build Brands and Help Them
+                  Succeed
                 </div>
                 <div className="services__block">
                   <img
@@ -151,30 +139,23 @@ export const App = () => {
           <div className="project__inner">
             <hr className="project__line" />
             <div className="project__block">
+              <div className="project__text">We Have Some Awesomr Project.</div>
               <div className="project__text">
-                We Have Some Awesomr Project.
-              </div>
-              <div className="project__text">
-                Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry. Lorem Ipsum has been the industry's
-                standard dummy text ever since the 1500s,
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
               </div>
             </div>
           </div>
 
           <div className="product">
             <div className="product__inner">
-              <img
-                className="product__picture"
-                src={bitmap}
-                alt="bitmap"
-              />
+              <img className="product__picture" src={bitmap} alt="bitmap" />
               <div className="product__banner">
                 <div className="product__banner-text">
                   “In my history of working with trade show vendors, I can
                   honestly say that there is not one company that I've ever
-                  worked with that has better service than Exhibit
-                  Systems.”
+                  worked with that has better service than Exhibit Systems.”
                 </div>
                 <div className="product__banner-block">
                   <img
@@ -193,16 +174,13 @@ export const App = () => {
                 </div>
               </div>
               <div className="product__block">
+                <div className="product__text">The wall new Balenciaga.com</div>
                 <div className="product__text">
-                  The wall new Balenciaga.com
-                </div>
-                <div className="product__text">
-                  This article is floated online with an aim to help you
-                  find the best dvd printing solution.
+                  This article is floated online with an aim to help you find
+                  the best dvd printing solution.
                   <br />
-                  <br /> Dvd printing is an important feature used by large
-                  and small DVD production houses to print information on
-                  DVDs.
+                  <br /> Dvd printing is an important feature used by large and
+                  small DVD production houses to print information on DVDs.
                 </div>
                 <div className="product__text">▶ See Case Study</div>
                 <div className="product__underline"></div>
@@ -217,10 +195,9 @@ export const App = () => {
               />
               <div className="product__banner product-reverse">
                 <div className="product__banner-text">
-                  “I know they are going to be honest with me. I am not
-                  going to get a subpar product; I know it is going to be
-                  good. That is the number one advantage of working with
-                  Exhibit Systems.
+                  “I know they are going to be honest with me. I am not going to
+                  get a subpar product; I know it is going to be good. That is
+                  the number one advantage of working with Exhibit Systems.
                 </div>
                 <div className="product__banner-block">
                   <img
@@ -244,11 +221,11 @@ export const App = () => {
                 </div>
                 <div className="product__text">
                   While it was just a TV show, that little speech at the
-                  beginning of the original Star Trek show really did do a
-                  good job of capturing our feelings about space.
+                  beginning of the original Star Trek show really did do a good
+                  job of capturing our feelings about space.
                   <br />
-                  <br /> It is those feelings that drive our love of
-                  astronomy and our desire to learn more and more about it.
+                  <br /> It is those feelings that drive our love of astronomy
+                  and our desire to learn more and more about it.
                 </div>
                 <div className="product__text">▶ See Case Study</div>
                 <div className="product__underline"></div>
@@ -263,9 +240,9 @@ export const App = () => {
               />
               <div className="product__banner product-reverse-3">
                 <div className="product__banner-text">
-                  “Exhibit Systems is not a provider. They are not a
-                  supplier. They are a partner with expertise in making
-                  trade shows effective.”
+                  “Exhibit Systems is not a provider. They are not a supplier.
+                  They are a partner with expertise in making trade shows
+                  effective.”
                 </div>
                 <div className="product__banner-block product-reverse-text">
                   <img
@@ -274,9 +251,7 @@ export const App = () => {
                     alt="people"
                   />
                   <div className="product__banner-block-slogans center-3">
-                    <div className="product__banner-slogan">
-                      Eunice Oliver
-                    </div>
+                    <div className="product__banner-slogan">Eunice Oliver</div>
                     <div className="product__banner-slogan">
                       Founder & CEO, Zeem
                     </div>
@@ -290,15 +265,13 @@ export const App = () => {
                   <span className="left-text">Online Booking</span>
                 </div>
                 <div className="product__text left-text">
-                  You should be able to find several indispensable facts
-                  about motivation in the following paragraphs.
+                  You should be able to find several indispensable facts about
+                  motivation in the following paragraphs.
                   <br />
-                  <br /> If there’s at least one fact you didn’t know
-                  before, imagine the difference it might make.
+                  <br /> If there’s at least one fact you didn’t know before,
+                  imagine the difference it might make.
                 </div>
-                <div className="product__text left-text">
-                  ▶ See Case Study
-                </div>
+                <div className="product__text left-text">▶ See Case Study</div>
                 <div className="product__underline"></div>
               </div>
             </div>
@@ -319,8 +292,8 @@ export const App = () => {
                   Product <br /> Design Sprint
                 </div>
                 <div className="evaluation__text">
-                  A five-day workshop that will help you answer crucial
-                  business questions
+                  A five-day workshop that will help you answer crucial business
+                  questions
                 </div>
                 <a
                   className="evaluation__link  evaluation__link--active"
@@ -363,8 +336,8 @@ export const App = () => {
                   <br /> Review
                 </div>
                 <div className="evaluation__text">
-                  Do you know what one of the key secrets of success is
-                  what makes people successful in business
+                  Do you know what one of the key secrets of success is what
+                  makes people successful in business
                 </div>
                 <a className="evaluation__link" href="/#">
                   ▶ Read More
@@ -388,9 +361,7 @@ export const App = () => {
                 <div className="footer__call">
                   <div className="footer__info-block">
                     <div className="footer__phone">Phone</div>
-                    <div className="footer__phone-name">
-                      +32 50 31 28 32
-                    </div>
+                    <div className="footer__phone-name">+32 50 31 28 32</div>
                   </div>
                   <div className="footer__info-block">
                     <div className="footer__address">Address</div>
@@ -407,18 +378,10 @@ export const App = () => {
               <div className="footer__description-block">
                 <div className="footer__paragraph">Services</div>
                 <ul className="footer__paragraph-list">
-                  <li className="footer__paragraph-text">
-                    Strategy Design
-                  </li>
-                  <li className="footer__paragraph-text">
-                    Product Design
-                  </li>
-                  <li className="footer__paragraph-text">
-                    Content Strategy
-                  </li>
-                  <li className="footer__paragraph-text">
-                    Brand Strategy
-                  </li>
+                  <li className="footer__paragraph-text">Strategy Design</li>
+                  <li className="footer__paragraph-text">Product Design</li>
+                  <li className="footer__paragraph-text">Content Strategy</li>
+                  <li className="footer__paragraph-text">Brand Strategy</li>
                   <li className="footer__paragraph-text">Development</li>
                 </ul>
               </div>
@@ -427,12 +390,8 @@ export const App = () => {
                 <div className="footer__paragraph">Help And Advice</div>
                 <ul className="footer__paragraph-list">
                   <li className="footer__paragraph-text">How it works</li>
-                  <li className="footer__paragraph-text">
-                    Contact Support
-                  </li>
-                  <li className="footer__paragraph-text">
-                    Privacy Policy
-                  </li>
+                  <li className="footer__paragraph-text">Contact Support</li>
+                  <li className="footer__paragraph-text">Privacy Policy</li>
                   <li className="footer__paragraph-text">FAQ</li>
                 </ul>
               </div>
@@ -464,7 +423,7 @@ export const App = () => {
                 </div>
               </div>
             </div>
-            
+
             <hr className="footer__under-line" />
 
             <div className="footer-under__block">
@@ -488,11 +447,7 @@ export const App = () => {
                     />
                   </div>
                   <img className="logo__text" src={webovio} alt="text" />
-                  <img
-                    className="logo__point"
-                    src={webovioPoint}
-                    alt="point"
-                  />
+                  <img className="logo__point" src={webovioPoint} alt="point" />
                 </div>
               </div>
               <div className="footer-under__text">
